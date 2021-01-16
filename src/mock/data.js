19cd70af2d12ec62b93788b5db5bc4f92b453e3d
Kabir Kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2021-01-16 12:01:15 +0530

    Modified Portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'My Portfolio', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -10,80 +10,62 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Kabir Kakkar',
+  subtitle: "I'm starting my journey on Web Development",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'me.jpg',
+  paragraphOne: 'Kabir is looking to build a professional foundation in Web Development, Machine Learning and Data Analysis. ',
+  paragraphTwo: 'He is a disciplined and industrious emerging professional with the drive and skills to make a positive impact.',
+  paragraphThree: 'Currently a final student at Manipal University Jaipur pursuing B.Tech',
+  resume: 'https://kabir-kakkar.github.io/Kabir%20Kakkar.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project1.jpg',
+    title: 'Demand Forecasting',
+    info: 'This Project uses MLlib to help enterprises predict sales and even get  a detailed analysis of their ongoing sales paradigm with the help of a BI tool.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/kabir-kakkar/Demand-Forecasting', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project2.jpg',
+    title: 'Predicting Age and Gender Using OpenCV',
+    info: 'This Project uses a Wide Residual Network and opens a real time webcam through which the Network determines the Age and Gender of the person',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    repo: 'https://github.com/kabir-kakkar/Age-and-Gender-Prediction', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'kabir.kakkar4@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/kabir-kakkar/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/kabir-kakkar',
     },
   ],
 };
